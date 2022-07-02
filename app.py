32c2e25d82b2a9107133cab8d4385529ab58f3e8
Jiayi Xu <xujiayi_1256@me.com>
2022-07-02 14:21:09 +0800

    code ready to be deployed to production

--- app.py
+++ app.py
@@ -1,33 +1,28 @@
 from datetime import datetime
+
+import requests
 import streamlit as st
 # importing geopy library
 from geopy.geocoders import Nominatim
-import requests
 
 # calling the Nominatim tool
 loc = Nominatim(user_agent="GetLoc")
-
-
-
-
 
 '''
 # TaxiFareModel front
 '''
 
-
-
-
 d = st.date_input("Pickup Date")
 t = st.time_input('Pickup Time')
-dt = datetime.combine(d,t)
+dt = datetime.combine(d, t)
 
 print(dt)
 
 dt = datetime.strftime(dt, '%Y-%m-%d %H:%M:%S')
 st.write('Pickup time:', dt)
 
-pickup = st.text_input('Pickup Location',placeholder='Enter your pickup location',value='Madison Square Garden, New York')
+pickup = st.text_input('Pickup Location', placeholder='Enter your pickup location',
+                       value='Madison Square Garden, New York')
 if pickup:
     pickupLoc = loc.geocode(pickup)
     if pickupLoc:
@@ -35,7 +30,8 @@ if pickup:
         st.write("Latitude = ", pickupLoc.latitude, "\n")
         st.write("Longitude = ", pickupLoc.longitude)
 
-dropoff = st.text_input('Dropoff Location',placeholder='Enter your dropoff location',value='Trump Tower, New York')
+dropoff = st.text_input('Dropoff Location', placeholder='Enter your dropoff location',
+                        value='Trump Tower, New York')
 if dropoff:
     dropoffLoc = loc.geocode(dropoff)
     if dropoffLoc:
@@ -47,15 +43,15 @@ passenger = st.slider('passenger count', 1, 4, 1)
 
 if st.button('Predict'):
     data = {
-    'pickup_datetime': dt,
-    'pickup_longitude': pickupLoc.longitude,
-    'pickup_latitude': pickupLoc.latitude,
-    'dropoff_longitude': dropoffLoc.longitude,
-    'dropoff_latitude': dropoffLoc.latitude,
-    'passenger_count': passenger
+        'pickup_datetime': dt,
+        'pickup_longitude': pickupLoc.longitude,
+        'pickup_latitude': pickupLoc.latitude,
+        'dropoff_longitude': dropoffLoc.longitude,
+        'dropoff_latitude': dropoffLoc.latitude,
+        'passenger_count': passenger
     }
     response = requests.get("https://taxifare.lewagon.ai/predict", params=data)
     if response.ok:
-        st.header('Fare: $'+str(round(response.json()['fare'],2)))
+        st.header('Fare: $' + str(round(response.json()['fare'], 2)))
     elif response.status_code == 400:
         st.header("Something went wrong")
